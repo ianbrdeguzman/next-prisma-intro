991c532089b4ef17fa8b1d90ce3c21cd7cc96d39
ianbrdeguzman <ianbrdeguzman@gmail.com>
2022-01-16 10:00:45 -0500

    add form component

---
+++ components/Form.tsx
@@ -0,0 +1,59 @@
+import { useState } from 'react';
+import styles from '../styles/components/Form.module.css';
+
+interface FormValues {
+  [key: string]: string;
+}
+
+export function Form() {
+  const [formValues, setFormValues] = useState<FormValues>({});
+
+  const handleOnChange = (e: React.FormEvent<HTMLInputElement>) => {
+    setFormValues({
+      ...formValues,
+      [e.currentTarget.name]: e.currentTarget.value
+    });
+  };
+
+  return (
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        if (Object.keys(formValues).length < 4) return;
+        alert(JSON.stringify(formValues, null, 2));
+      }}
+    >
+      <input
+        className={styles.input}
+        type="text"
+        name="first"
+        placeholder="First Name"
+        onChange={handleOnChange}
+      />
+      <input
+        className={styles.input}
+        type="text"
+        name="last"
+        placeholder="Last Name"
+        onChange={handleOnChange}
+      />
+      <input
+        className={styles.input}
+        type="text"
+        name="email"
+        placeholder="Email"
+        onChange={handleOnChange}
+      />
+      <input
+        className={styles.input}
+        type="text"
+        name="avatar"
+        placeholder="Avatar"
+        onChange={handleOnChange}
+      />
+      <button className={styles.submit} type="submit">
+        Submit
+      </button>
+    </form>
+  );
+}
