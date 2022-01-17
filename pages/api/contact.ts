fcf855c6b330c97f206434a5d3965803183a145a
ianbrdeguzman <ianbrdeguzman@gmail.com>
2022-01-17 08:02:20 -0500

    added contact api route

--- pages/api/contact.ts
+++ pages/api/contact.ts
@@ -3,5 +3,10 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method !== 'POST')
+    return res.status(405).json({ message: 'This is not a POST request' });
+
+  const newContact = req.body;
+  console.log(newContact);
   res.status(200).json({ name: 'John Doe' });
 }
